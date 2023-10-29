127d29d8615f78d537f1bdd03b3838a331842792
yxl <1660855825@qq.com>
2023-10-29 21:01:16 +0800

    个人信息页面退出登录方法

--- src/pages/page-personal/main.js
+++ src/pages/page-personal/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import jsCookie from 'js-cookie'
 
 Vue.config.productionTip = false
 Vue.prototype.axios = axios
+Vue.prototype.$cookie = jsCookie
 
 new Vue({
   axios,
