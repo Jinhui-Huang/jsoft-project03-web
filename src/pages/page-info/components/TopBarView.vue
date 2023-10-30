0ef15c7a8603c675a6d4054441f788950ba9ce8a
yxl <1660855825@qq.com>
2023-10-30 14:11:14 +0800

    fix: 调整界面

--- src/pages/page-info/components/TopBarView.vue
+++ src/pages/page-info/components/TopBarView.vue
@@ -6,16 +6,25 @@
                     <img src="/static/images/logo_1.png" />
                     <img src="/static/images/logo_2.png" />
                 </a>
-                <span>www.xxx.com</span>
-            </div>
-            <div class="header_c">
-                <input name="" type="text" placeholder="请输入关键字..." class="keyword" />
-                <input name="" type="button" class="btnsearch" />
+                
             </div>
             <div class="header_r">
-                <a href="/login">个人登录</a>
+                <a href="/personal/resume">{{ userName }}</a>
                 <a href="/login/register">注册</a>
             </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            userName:null,
+        }
+    },
+    mounted(){
+        this.userName = this.$cookie.get("cookieUserName")
+        console.log("获取到的userName"+this.userName)
+    }
+}
+</script>
